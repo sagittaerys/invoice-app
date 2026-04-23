70057ca9558a6b42e8f197873b06d37411dae445
Olamilekan Aremu <aremu0235@gmail.com>
2026-04-23 21:33:06 +0100

    chore: refined logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -54,13 +54,7 @@ export const Sidebar = () => {
       >
         {/* Logo */}
         <div className="relative flex items-center justify-center flex-shrink-0 overflow-hidden" style={{ width: 72, height: 72, borderRadius: '0 20px 20px 0' }}>
-          <div className="absolute inset-0" style={{ backgroundColor: '#7C5DFA', borderRadius: '0 20px 0 0' }} />
-          <div className="absolute bottom-0 left-0 right-0" style={{ height: '50%', backgroundColor: '#9277FF', borderRadius: '20px 0 20px 0' }} />
-          <span className="relative z-10">
-            <svg width="21" height="20" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.3657 8.00275C13.5611 7.50895 14.2612 7.50895 14.4566 8.00275L15.8976 11.7087C15.9892 11.9462 16.2142 12.1055 16.4679 12.1185L20.4289 12.3227C20.9574 12.3499 21.172 13.0135 20.7578 13.3584L17.6793 15.8769C17.4806 16.0398 17.3965 16.3008 17.464 16.5459L18.5111 20.3681C18.6513 20.8782 18.0849 21.2877 17.6378 20.9987L14.2676 18.8615C14.0536 18.7277 13.7687 18.7277 13.5547 18.8615L10.1845 20.9987C9.73737 21.2877 9.17098 20.8782 9.31121 20.3681L10.3583 16.5459C10.4258 16.3008 10.3417 16.0398 10.143 15.8769L7.06448 13.3584C6.65025 13.0135 6.86488 12.3499 7.39339 12.3227L11.3544 12.1185C11.6081 12.1055 11.8331 11.9462 11.9247 11.7087L13.3657 8.00275Z" fill="white"/>
-            </svg>
-          </span>
+          <img src="/logo.png" alt="logo" />
         </div>
 
         {/* Right controls */}
